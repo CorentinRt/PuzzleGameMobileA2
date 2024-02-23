dc66258660b14a47b7f9bcfbcdc2624c3026c51c
Corentinrmt <corentin.remot@gmail.com>
2024-02-23 23:57:41 +0100

    Start basis of Players animation manager

---
+++ PuzzleGameA2/Assets/Scripts/Corentin/Animation/PlayersAnimationManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersAnimationManager : MonoBehaviour
+{
+    private Animator _animator;
+
+    public Animator Animator { get => _animator; set => _animator = value; }
+
+    public void PlayWalkAnimation()
+    {
+
+    }
+    public void PlayRunAnimation()
+    {
+
+    }
+    public void PlayJumpAnimation()
+    {
+
+    }
+
+
+    public void PlayWinAnimation()
+    {
+
+    }
+    public void PlayDeathAnimation()
+    {
+
+    }
+}
