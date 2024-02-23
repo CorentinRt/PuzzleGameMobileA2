04464e34850b6d568186e844e58ca6a523e8def4
August <augustlanguepin@gmail.com>
2024-02-23 16:25:12 +0100

    scene

--- PuzzleGameA2/Assets/Scripts/August/ProjectileDown.cs
+++ PuzzleGameA2/Assets/Scripts/August/ProjectileDown.cs
@@ -7,12 +7,6 @@ public class ProjectileDown : MonoBehaviour
 
     public float projectileSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
