fec602ac2968f6197b75865360fe8e70da0e25bd
August <augustlanguepin@gmail.com>
2024-02-23 11:08:18 +0100

    arrow change

--- PuzzleGameA2/Assets/Scripts/August/ProjectileArrow.cs
+++ PuzzleGameA2/Assets/Scripts/August/ProjectileArrow.cs
@@ -6,19 +6,12 @@ public class ProjectileArrow : MonoBehaviour
 {
     //Arrow speed
     public float arrowSpeed;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+ 
     // Update is called once per frame
     void Update()
     {
         //Arrow movement
         transform.Translate(Vector3.up * arrowSpeed * Time.deltaTime);
-
 
     }
 }
