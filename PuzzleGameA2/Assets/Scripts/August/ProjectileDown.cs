e5b852965c9e9d777d625d141a27f68de93d82bb
August <augustlanguepin@gmail.com>
2024-02-26 11:24:21 +0100

    Removed Projectile Arrow/ Added Destroy on collide
    Added Collision and Destroy Gameobject on collide for Projectile

--- PuzzleGameA2/Assets/Scripts/August/ProjectileDown.cs
+++ PuzzleGameA2/Assets/Scripts/August/ProjectileDown.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class ProjectileDown : MonoBehaviour
 {
-
     public float projectileSpeed;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * projectileSpeed * Time.deltaTime);
+    }
 
+    //Destroy the projectile when it collides with player
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
