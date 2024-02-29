ad8d6c28bf0ec5506cedf201bc18ffbea9b9d554
Corentinrmt <corentin.remot@gmail.com>
2024-02-29 19:45:03 +0100

    start repare branch

--- PuzzleGameA2/Assets/Scripts/Enums.cs
+++ PuzzleGameA2/Assets/Scripts/Enums.cs
@@ -20,7 +20,8 @@ namespace Enums
         PlateformePlacement = 0,
         PlayersMoving = 1,
         ChoicePhase = 2,
-        GameEndPhase = 3
+        GameEndPhase = 3,
+        GameOver = 4
     }
     
     public enum ShapeType
