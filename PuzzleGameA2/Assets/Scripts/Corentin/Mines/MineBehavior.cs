770b4beea715930ed12f5e7479ecb1cfc82f9378
Corentinrmt <corentin.remot@gmail.com>
2024-03-01 22:39:34 +0100

    start debug mine explosion

--- PuzzleGameA2/Assets/Scripts/Corentin/Mines/MineBehavior.cs
+++ PuzzleGameA2/Assets/Scripts/Corentin/Mines/MineBehavior.cs
@@ -8,22 +8,37 @@ public class MineBehavior : MonoBehaviour
     
     public bool HasExplode { get => _hasExplode; set => _hasExplode = value; }
 
+    private void Update()
+    {
+
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && GameManager.Instance.CurrentPhase == Enums.PhaseType.PlayersMoving)
+        if (collision.CompareTag("Player"))
         {
             if (!_hasExplode)
             {
                 if (collision.TryGetComponent<CorpsesBehavior>(out CorpsesBehavior corpsesBehavior))
                 {
-                    //_hasExplode = true;
-                    AddExplosionForce(collision.GetComponent<Rigidbody2D>(), 700f, transform.position + new Vector3(0f, -0.5f), 5f);
-                    Debug.Log("Corpse take mine");
+                    _hasExplode = true;
+                    Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 10f);
+                    foreach (var item in colliders)
+                    {
+                        if (item.TryGetComponent<CorpsesBehavior>(out CorpsesBehavior corpsesBehavior1))
+                        {
+                            AddExplosionForce(item.GetComponent<Rigidbody2D>(), 700f, transform.position + new Vector3(0f, -0.5f), 10f);
+                            Debug.Log("Corpse take mine");
+                        }
+                    }
                 }
-                if (collision.TryGetComponent<PlayerBehaviour>(out PlayerBehaviour playerBehaviour))
+                if (GameManager.Instance.CurrentPhase == Enums.PhaseType.PlayersMoving)
                 {
-                    playerBehaviour.StepOnMine(transform.position);
-                    Debug.Log("Player take mine");
+                    if (collision.TryGetComponent<PlayerBehaviour>(out PlayerBehaviour playerBehaviour))
+                    {
+                        playerBehaviour.StepOnMine(transform.position);
+                        Debug.Log("Player take mine");
+                    }
                 }
             }
         }
