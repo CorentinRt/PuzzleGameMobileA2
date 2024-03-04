18c19bace1a92fea2ed6d4be07b46d9d4eb56e27
Corentinrmt <corentin.remot@gmail.com>
2024-03-04 20:04:42 +0100

    Change direction selection system

--- PuzzleGameA2/Assets/Scripts/Corentin/ShapeSelector/ShapeGetter.cs
+++ PuzzleGameA2/Assets/Scripts/Corentin/ShapeSelector/ShapeGetter.cs
@@ -23,6 +23,8 @@ public class ShapeGetter : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     [SerializeField] private ShapePower _shapePower;
 
+    [SerializeField] private bool _isLookingLeft;
+
     [SerializeField] private List<GameObject> _powerVisuals;
 
     [SerializeField] private bool _isAffectedByGravity;
@@ -32,6 +34,8 @@ public class ShapeGetter : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public int ShapeCount { get => _shapeCount; set => _shapeCount = value; }
     public ShapePower ShapePower { get => _shapePower; set => _shapePower = value; }
     public ShapeType ShapeType { get => _shapeType; set => _shapeType = value; }
+    public bool IsLookingLeft { get => _isLookingLeft; set => _isLookingLeft = value; }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         if (_shapeCount > 0)
@@ -44,6 +48,13 @@ public class ShapeGetter : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             if (_isAffectedByGravity)
             {
                 tempShape.GetComponentInChildren<ShapeManagerNoCanvas>().IsAffectedByGravity = true;
+            }
+            if (_isLookingLeft)
+            {
+                tempShape.GetComponent<ShapeManagerNoCanvas>().ChangeDirection();
+                Vector3 tempVect = tempShape.transform.localScale;
+                tempVect.x = -1;
+                tempShape.transform.localScale = tempVect;
             }
             tempShape.GetComponent<ShapeManagerNoCanvas>().SetShapePower(_shapePower);
             tempShape.GetComponentInChildren<DragDropNoCanvas>().Dragging = true;
@@ -223,6 +234,14 @@ public class ShapeGetter : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         _shapeCount = shape.MaxCount;
         _shapePower = shape.Power;
         _shapeType = shape.Type;
+        _isLookingLeft = shape.IsLookingLeft;
+        if (_isLookingLeft)
+        {
+            Vector3 tempVect = transform.localScale;
+            tempVect.x = -1;
+            transform.localScale = tempVect;
+            _shapeCountText.transform.localScale = tempVect;
+        }
     }
 
     private void OnDestroy()
