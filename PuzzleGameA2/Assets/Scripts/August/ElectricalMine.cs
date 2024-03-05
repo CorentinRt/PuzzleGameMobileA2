965d58a08140344e7f33869f8f5c5e953480e25a
August Languepin <augustlanguepin@gmail.com>
2024-03-05 09:44:28 +0100

    ElectricalMine

---
+++ PuzzleGameA2/Assets/Scripts/August/ElectricalMine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ElectricalMine : MonoBehaviour
+{
+    public float activationRadius = 5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
