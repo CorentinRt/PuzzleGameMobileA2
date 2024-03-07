aa7ac6805de508887beefe5557e5ed83b9b0fdd6
August <augustlanguepin@gmail.com>
2024-03-07 11:47:36 +0100

    Achievement Manager WIP

--- PuzzleGameA2/Assets/Scripts/August/AchievementManager.cs
+++ PuzzleGameA2/Assets/Scripts/August/AchievementManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class AchievementManager : MonoBehaviour
 {
+
+    public GameObject achievementPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateAchievement("General");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreateAchievement(string category)
+    {
+        GameObject achievement = (GameObject)Instantiate(achievementPrefab);
+        SetAchievementInfo(category, achievement);
+    }
+
+    public void SetAchievementInfo(string category, GameObject achievement)
+    {
+        achievement.transform.SetParent(GameObject.Find(category).transform);
     }
 }
