de5add8122a1ef29e9959bd11b6332d2c73719db
Corentinrmt <corentin.remot@gmail.com>
2024-03-12 11:31:39 +0100

    Start soundManager and sound parameters system

--- PuzzleGameA2/Assets/Scripts/Corentin/SoundManager.cs
+++ PuzzleGameA2/Assets/Scripts/Corentin/SoundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
@@ -12,26 +13,64 @@ public class SoundManager : MonoBehaviour
     {
         if (_instance != null)
         {
-            Destroy(_instance);
+            Destroy(gameObject);
         }
         _instance = this;
 
         _audioSource = GetComponent<AudioSource>();
     }
 
+    [SerializeField] private float _musicVolume;
+    [SerializeField] private float _effectVolume;
 
-    [SerializeField] private List<AudioClip> _menuMusics;
-    [SerializeField] private List<AudioClip> _inGameMusics;
+    [SerializeField] private AudioClip _menuMusic;
+    [SerializeField] private AudioClip _inGameMusic;
 
     [SerializeField] private AudioClip _victorySound;
     [SerializeField] private AudioClip _defeatSound;
 
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("_volumeMusic"))
+        {
+            PlayerPrefs.SetFloat("_volumeMusic", 0.5f);
+        }
+        if (!PlayerPrefs.HasKey("_effectVolume"))
+        {
+            PlayerPrefs.SetFloat("_effectVolume", 0.5f);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (PlayerPrefs.HasKey("_musicVolume"))
+        {
+            _musicVolume = PlayerPrefs.GetFloat("_musicVolume");
+        }
+        if (PlayerPrefs.HasKey("_effectVolume"))
+        {
+            _effectVolume = PlayerPrefs.GetFloat("_effectVolume");
+        }
+
+        if (SceneManager.GetActiveScene().name == "MainMenuScene" && _audioSource.clip != _menuMusic)
+        {
+            _audioSource.clip = _menuMusic;
+            _audioSource.Play();
+        }
+        else if (SceneManager.GetActiveScene().name != "MainMenuScene" && _audioSource.clip != _inGameMusic)
+        {
+            _audioSource.clip = _inGameMusic;
+            _audioSource.Play();
+        }
+    }
+
     public void PlayVictorySound()
     {
-        _audioSource.PlayOneShot(_victorySound);
+        _audioSource.PlayOneShot(_victorySound, _effectVolume);
     }
     public void PlayDefeatSound()
     {
-        _audioSource.PlayOneShot(_defeatSound);
+        _audioSource.PlayOneShot(_defeatSound, _effectVolume);
     }
 }
